1181651675f32091da9e83f131ea45b10914f5f7
Md. Abu Rayhan Mondal <mdaburayhanmondal@gmail.com>
2026-02-14 01:55:25 +0600

    updated cors policies

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173/',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   }),
 );
 
